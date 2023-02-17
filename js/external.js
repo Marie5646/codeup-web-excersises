53e37bcda91db13591dfd448cc070caafc5162b9
Asia Autrey <asia.autrey0@gmail.com>
2023-02-17 12:24:19 -0600

    js with html lecture notes

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("this is my external js file");
+
+alert("Have a good lunch");
+
+let submit= confirm("Are you sure you want to resubmit this form");
+console.log(submit);
+
+let age =prompt(message"what is your age?")
+alert("wow you're old.You're"+ age);
